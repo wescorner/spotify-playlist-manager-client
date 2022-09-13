4e6a6da99da2ca2877d68d43e6330766d1e76b64
Wesley Corner <wescorner@gmail.com>
2022-09-13 14:58:29 -0400

    placeholder images for playlist/stats pages

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -4,7 +4,6 @@ import "./CategoryCard.scss";
 export default function CategoryCard({ image, description, title, totalPlaylists, onClick }) {
   return (
     <Card className="categoryCard" onClick={onClick}>
-      {/* <Card.Img className="category-card-img" variant="top" src={image} /> */}
       <Card.Body>
         <img className="category-card-img" src={image} alt="icon" />
         <Card.Title>{title}</Card.Title>
