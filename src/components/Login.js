b7099709917ec7217102f61fb5cae68401582483
Wesley Corner <wescorner@gmail.com>
2022-09-13 22:08:30 -0400

    test3

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,7 @@ export default function Login({ setLoggingIn }) {
       <h1 className="title">Playlist Manager</h1>
       <a
         className="login"
-        href="https://playlist-manager-server.herokuapp.com/login"
+        href="https://api.spotifyplaylistmanager.com/login"
         onClick={setLoggingIn}
       >
         {" "}
